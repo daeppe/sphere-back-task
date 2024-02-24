565aa387cdc36000ccbff053859e832489a3882b
Epichin <d.pena@hotmail.com>
2024-02-24 00:01:02 -0300

    Feat(database): Add database configuration files.

---
+++ src/database/data-source.ts
@@ -0,0 +1,18 @@
+import { DataSource } from 'typeorm';
+
+const DB_HOST = String(process.env.DB_HOST);
+const DB_PORT = Number(process.env.DB_PORT);
+const DB_USERNAME = String(process.env.DB_USERNAME);
+const DB_PASSWORD = String(process.env.DB_PASSWORD);
+const DB_DATABASE = String(process.env.DB_DATABASE);
+
+export const PostgresDataSource = new DataSource({
+    type: 'postgres',
+    host: DB_HOST,
+    port: DB_PORT,
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_DATABASE,
+    entities: ['src/database/entities/*.ts'],
+    migrations: ['src/database/migrations/*.ts']
+});
